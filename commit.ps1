ed0635a10e9924140f37eeca047e449118452b74
Callum Nicoll <xcallumnicx@outlook.com>
2025-12-06 14:26:22 +0000

    Update Commit script with licensing information and added support to commit AND push at the same time
    Signed-off-by: Callum Nicoll <xcallumnicx@outlook.com>

--- commit.ps1
+++ commit.ps1
@@ -1,6 +1,16 @@
+# THIS FILE IS A PART OF FORGEBUILD
+# FORGEBUILD IS LICENSED UNDER THE MIT LICENSE.
+# SEE MORE DETAILS AT https://opensource.org/licenses/MIT
+# OR THE LICENSE FILE IN THE ROOT DIRECTORY OF THIS PROJECT.
+# OR THE GITHUB REPOSITORY AT https://github.com/glkdrlgkrlzflnjkgh/ForgeBuild
 param(
-    [string]$Message = "Utility commit"
+    [string]$Message = "Utility commit",
+    [bool]$Push = $false
 )
 
 # Stage all tracked changes and commit with Signed-off-by
 git commit -s -a -m $Message
+
+if ($Push) {
+    git push
+}
